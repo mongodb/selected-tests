c0541f180a09e7c1bdfa0f40d9ee05de7512c187
Cameron Brill <c00brill@gmail.com>
2019-10-01 16:32:54 -0500

    Fixed test_health_controller data comparison.

--- tests/selectedtests/app/controllers/test_health_controller.py
+++ tests/selectedtests/app/controllers/test_health_controller.py
@@ -7,4 +7,5 @@ def test_health_endpoint(app_client: testing.FlaskClient):
     """
     response = app_client.get("/health")
     assert response.status_code == 200
-    assert "online" and "true" in response.get_data(as_text=True)
+    res_data = response.get_data(as_text=True)
+    assert "online" in res_data and "true" in res_data
