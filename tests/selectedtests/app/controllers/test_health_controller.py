5263fd88b2145e743140f4780bee4db441f5e58d
lydiastepanek <lydia.stepanek@mongodb.com>
2019-12-03 16:09:31 -0500

    Fix health controller test (#27)

--- tests/selectedtests/app/controllers/test_health_controller.py
+++ tests/selectedtests/app/controllers/test_health_controller.py
@@ -7,4 +7,5 @@ def test_health_endpoint(app_client: testing.FlaskClient):
     """
     response = app_client.get("/health")
     assert response.status_code == 200
-    assert "online" and "true" in response.get_data(as_text=True)
+    response_data = response.get_json()
+    assert "online" in response_data and response_data["online"]
