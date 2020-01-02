01fb584d307ac45f405b2d585fe740faff76847d
Lydia Stepanek <lydia.stepanek@mongodb.com>
2020-01-02 15:18:36 -0500

    MAKE-1070 Task mapping creation should set repo value to module's repo name for module-related task mappings

--- tests/selectedtests/task_mappings/test_create_task_mappings.py
+++ tests/selectedtests/task_mappings/test_create_task_mappings.py
@@ -5,6 +5,7 @@ from datetime import date, datetime, time
 from unittest.mock import MagicMock, patch
 
 from selectedtests.task_mappings import create_task_mappings as under_test
+from selectedtests.task_mappings.create_task_mappings import ChangedFile
 from selectedtests.task_mappings.version_limit import VersionLimit
 
 NS = "selectedtests.task_mappings.create_task_mappings"
@@ -39,14 +40,17 @@ class TestFullRunThrough:
             MagicMock(identifier="fake_name"),
         ]
 
-        filtered_files_mock.return_value = ["src/file1", "src/file2"]
+        filtered_files_mock.return_value = {
+            ChangedFile("src/file1", "my_repo"),
+            ChangedFile("src/file2", "my_repo"),
+        }
 
         output, most_recent_version_analyzed = under_test.TaskMappings.create_task_mappings(
             mock_evg_api, project_name, version_limit_mock, re.compile("src.*")
         )
-
         transformed_out = output.transform()
-        assert expected_task_mappings_output == transformed_out
+        for task_mapping in transformed_out:
+            assert task_mapping in expected_task_mappings_output
         assert most_recent_version_analyzed == only_version_analyzed.version_id
 
 
@@ -62,7 +66,7 @@ class TestCreateTaskMappings:
         flipped_mock,
         module_changed_mock,
         associated_module_mock,
-        filtered_mock,
+        filtered_files_mock,
         diff_mock,
         get_evg_project_and_init_repo_mock,
     ):
@@ -86,11 +90,14 @@ class TestCreateTaskMappings:
         only_version_analyzed = evg_api_mock.versions_by_project.return_value[1]
 
         associated_module_mock.return_value = None
-        module_changed_mock.return_value = {"module_file"}
-        filtered_mock.return_value = {f"file{i}" for i in range(2)}
+        module_changed_mock.return_value = {ChangedFile("module_file", "my_module_repo")}
+        filtered_files_mock.return_value = {
+            ChangedFile("file0", "my_repo"),
+            ChangedFile("file1", "my_repo"),
+        }
 
         expected_file_list = deepcopy(module_changed_mock.return_value).union(
-            deepcopy(filtered_mock.return_value)
+            deepcopy(filtered_files_mock.return_value)
         )
 
         flipped_mock.return_value = {"variant1": ["task1", "task2"], "variant2": ["task3", "task4"]}
@@ -146,10 +153,13 @@ class TestCreateTaskMappings:
         # We still mock the two below so that we can check to make sure even if the version has a
         # module and that module has changed files in it, we don't actually add the changed files
         # to the task mappings if they're not asked for
-        module_changed_mock.return_value = ["module_file"]
+        module_changed_mock.return_value = {ChangedFile("module_file", "my_module_repo")}
         associated_module_mock.return_value = None
 
-        filtered_mock.return_value = [f"file{i}" for i in range(2)]
+        filtered_mock.return_value = {
+            ChangedFile("file0", "my_repo"),
+            ChangedFile("file1", "my_repo"),
+        }
         expected_file_list = deepcopy(filtered_mock.return_value)
 
         flipped_mock.return_value = {"variant1": ["task1", "task2"], "variant2": ["task3", "task4"]}
@@ -196,7 +206,10 @@ class TestCreateTaskMappings:
             MagicMock(create_time=datetime.combine(date(1, 1, 1), time(1, 2, i))) for i in range(3)
         ]
         evg_api_mock.versions_by_project.return_value.reverse()
-        filtered_mock.return_value = [f"file{i}" for i in range(2)]
+        filtered_mock.return_value = {
+            ChangedFile("file0", "my_repo"),
+            ChangedFile("file1", "my_repo"),
+        }
 
         flipped_mock.return_value = {}
         project_name = "project"
@@ -211,8 +224,8 @@ class TestCreateTaskMappings:
         )
 
         assert mappings.mappings == {
-            "file0": {"builds": {}, "seen_count": 1},
-            "file1": {"builds": {}, "seen_count": 1},
+            ChangedFile("file0", "my_repo"): {"builds": {}, "seen_count": 1},
+            ChangedFile("file1", "my_repo"): {"builds": {}, "seen_count": 1},
         }
 
     @patch(ns("_get_evg_project_and_init_repo"))
@@ -254,14 +267,14 @@ class TestTransformationOfTaskMappings:
     def test_basic_transformation(self):
         evergreen_project, repo_name, branch_name = "evergreen", "repo", "branch"
         task_mappings_dict = {
-            "src-file-0": {
+            ChangedFile("src-file-0", repo_name): {
                 "builds": {
                     "build-1": {"task-0": 1, "task-1": 1},
                     "build-2": {"task-3": 1, "task-4": 1},
                 },
                 "seen_count": 1,
             },
-            "src-file-1": {
+            ChangedFile("src-file-1", repo_name): {
                 "builds": {
                     "build-1": {"task-0": 1, "task-1": 1},
                     "build-2": {"task-3": 1, "task-4": 1},
@@ -269,9 +282,7 @@ class TestTransformationOfTaskMappings:
                 "seen_count": 1,
             },
         }
-        task_mappings = under_test.TaskMappings(
-            task_mappings_dict, evergreen_project, repo_name, branch_name
-        )
+        task_mappings = under_test.TaskMappings(task_mappings_dict, evergreen_project, branch_name)
         transformed_mappings = task_mappings.transform()
 
         assert len(transformed_mappings) == 2
@@ -292,12 +303,10 @@ class TestTransformationOfTaskMappings:
     def test_transform_files_with_no_builds_that_have_flipped_tasks(self):
         evergreen_project, repo_name, branch_name = "evergreen", "repo", "branch"
         task_mappings_dict = {
-            "src-file-0": {"builds": {}, "seen_count": 1},
-            "src-file-1": {"builds": {}, "seen_count": 1},
+            ChangedFile("src-file-0", repo_name): {"builds": {}, "seen_count": 1},
+            ChangedFile("src-file-1", repo_name): {"builds": {}, "seen_count": 1},
         }
-        task_mappings = under_test.TaskMappings(
-            task_mappings_dict, evergreen_project, repo_name, branch_name
-        )
+        task_mappings = under_test.TaskMappings(task_mappings_dict, evergreen_project, branch_name)
         transformed_mappings = task_mappings.transform()
 
         assert len(transformed_mappings) == 0
@@ -312,19 +321,19 @@ class TestFilteredFiles:
         changed_files_mock.side_effect = changed_files
 
         regex = re.compile("a.*")
-        filtered = under_test._get_filtered_files(None, regex)
+        filtered = under_test._get_filtered_files(None, regex, "my_repo")
 
         expected = ["a", "ab", "ac", "abc/test"]
 
         assert len(filtered) == 4
-        for letter in expected:
-            assert letter in filtered
+        for file in filtered:
+            assert file.file_name in expected
 
 
 class TestMapTasksToFiles:
     def test_basic_mapping(self):
         task_mappings = {}
-        changed_files = [f"test{i}" for i in range(5)]
+        changed_files = [ChangedFile(f"file{i}", "my_repo") for i in range(5)]
         flipped_tasks = {
             "build1": [f"task{i}" for i in range(5)],
             "build2": [f"task{i}" for i in range(5)],
@@ -347,7 +356,7 @@ class TestMapTasksToFiles:
 
     def test_adding_to_existing_mapping(self):
         task_mappings = {}
-        changed_files1 = [f"file{i}" for i in range(1)]
+        changed_files1 = [ChangedFile("file0", "my_repo")]
 
         small_task_list = [f"task{i}" for i in range(2)]
         large_task_list = [f"task{i}" for i in range(4)]
@@ -357,7 +366,7 @@ class TestMapTasksToFiles:
         under_test._map_tasks_to_files(changed_files1, flipped_tasks1, task_mappings)
 
         expected_after_first = {
-            "file0": {
+            ChangedFile("file0", "my_repo"): {
                 "builds": {"build1": {"task0": 1, "task1": 1}, "build2": {"task0": 1, "task1": 1}},
                 "seen_count": 1,
             }
@@ -365,7 +374,7 @@ class TestMapTasksToFiles:
 
         assert expected_after_first == task_mappings
 
-        changed_files2 = [f"file{i}" for i in range(2)]
+        changed_files2 = [ChangedFile("file0", "my_repo"), ChangedFile("file1", "my_repo")]
         flipped_tasks2 = {"build1": large_task_list, "build3": small_task_list}
 
         under_test._map_tasks_to_files(changed_files2, flipped_tasks2, task_mappings)
@@ -374,7 +383,7 @@ class TestMapTasksToFiles:
         expected_small_task_dict = {"task0": 1, "task1": 1}
         modified_large_task_dict = {"task0": 2, "task1": 2, "task2": 1, "task3": 1}
         expected_task_mappings = {
-            "file0": {
+            ChangedFile("file0", "my_repo"): {
                 "seen_count": 2,
                 "builds": {
                     "build1": modified_large_task_dict,
@@ -382,7 +391,7 @@ class TestMapTasksToFiles:
                     "build3": expected_small_task_dict,
                 },
             },
-            "file1": {
+            ChangedFile("file1", "my_repo"): {
                 "seen_count": 1,
                 "builds": {"build1": untouched_large_task_dict, "build3": expected_small_task_dict},
             },
