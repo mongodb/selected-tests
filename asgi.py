8c61ec43cd8859174c0a559b98dfa6aa98d5231e
Alexander Costas <alexander.costas@mongodb.com>
2020-01-24 10:27:08 -0500

    sort

--- asgi.py
+++ asgi.py
@@ -1,4 +1,4 @@
 from selectedtests.app.app import create_app
-from selectedtests.helpers import get_mongo_wrapper, get_evg_api
+from selectedtests.helpers import get_evg_api, get_mongo_wrapper
 
 app = create_app(get_mongo_wrapper(), get_evg_api())
