ed1ccf9a43af93533e04a0b00fdff0bfba5300d1
Alexander Costas <alexander.costas@mongodb.com>
2020-03-17 13:35:22 -0400

    script-update

--- cronjobs/lib/setup_ssh_keys.sh
+++ cronjobs/lib/setup_ssh_keys.sh
@@ -8,15 +8,23 @@ setup_ssh_keys() {
     chmod 600 "$target_dir"
   fi
 
+  if [ -d "$target_dir/selected_tests" ]; then
+    echo "$target_dir/selected_tests already exists"
+    exit 1
+  fi
+
   # Make sure we have the right keys for github.
   ssh-keyscan -t rsa github.com >> "$target_dir/known_hosts"
 
-  cat > "$target_dir/id_rsa" <<END_OF_PRIVATE
+  cat > "$target_dir/selected_tests" <<END_OF_PRIVATE
 $GITHUB_PRIVATE_KEY
 END_OF_PRIVATE
-  chmod 600 $target_dir/id_rsa
+  chmod 600 $target_dir/selected_tests
 
-  cat > "$target_dir/id_rsa.pub" <<END_OF_PUBLIC
+  cat > "$target_dir/selected_tests.pub" <<END_OF_PUBLIC
 $GITHUB_PUBLIC_KEY
 END_OF_PUBLIC
+
+  eval "$(ssh-agent -s)"
+  ssh-add -k ~/.ssh/selected_tests
 }
